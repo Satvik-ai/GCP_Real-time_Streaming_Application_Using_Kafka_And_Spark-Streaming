6c056872be009d0c0813f7344678b8da906bc43b
SATVIK CHANDRAKAR <chandrakarsatvik@gmail.com>
2025-04-17 23:02:28 +0530

    Delete Using_Seperate_Resources/resources/create_dataproc_cluster.sh

--- Using_Seperate_Resources/resources/create_dataproc_cluster.sh
+++
@@ -1,9 +0,0 @@
-gcloud dataproc clusters create satvik-cluster \
-    --region us-west1 \
-    --zone us-west1-a \
-    --single-node \
-    --master-machine-type n1-standard-4 \
-    --master-boot-disk-size 100GB \
-    --image-version 2.2-debian12 \
-    --project eminent-crane-448810-s3 \
-    --initialization-actions=gs://satvik-storage-bucket/install_dependencies.sh
